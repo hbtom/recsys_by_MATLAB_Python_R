9b07698a21420f4ecb4bd57a0fb7603f5e1c5434
yasdel <yashar.deldjoo@gmail.com>
2017-07-21 17:51:22 +0200

    Updated urm_split function
    + split_urm function was reporting train and test urm opposite way. it was fixed.
    
    - split urm was modfied to do the splitting based item splitting as well.

--- MATLAB/utils/demo_split_urm.m
+++ MATLAB/utils/demo_split_urm.m
@@ -2,14 +2,20 @@ clear
 clc
 close all
 
-urm = readtable('/Users/yashar/Documents/RecSys_YD/MATLAB/data/ml-20m/ratings.csv');
-output_fold = '/Users/yashar/Documents/RecSys_YD/MATLAB/utils/test/';
+input_rating_file = '/Users/yashar/Documents/GitHub/data/inputs/ml-20m/ratings.csv';
+      output_fold = '/Users/yashar/Documents/GitHub/data/outputs/urms/item';
+
+urm = readtable(input_rating_file);
 
 %load urm_ML20m.mat
 
-         k = 5  ;
-split_type = 'rating';
-   pop_thr =  0 ; 
+         k = 10  ;
+split_type = 'item';
+
+pop_thrList =  [0 0.001 0.005 0.01 0.05 0.1 0.2];
+
+
+for i = 1 :length(pop_thrList)    
+    pop_thr = pop_thrList(i);    
     split_urm(urm, split_type, k, pop_thr,output_fold)
-
-
+end
--- MATLAB/utils/split_urm.m
+++ MATLAB/utils/split_urm.m
@@ -22,7 +22,9 @@ n_i = length(unique(itemIds));
 n_r = length(ratings);
 den = n_r/(n_u*n_i);
 
+fprintf('******************************* \n');
 fprintf('*** Original URM statistics *** \n');
+fprintf('******************************* \n');
 fprintf('Number of unique users: %g \n',n_u)
 fprintf('Number of unique items: %g \n',n_i)
 fprintf('Number of ratings: %d \n',n_r)
@@ -50,8 +52,10 @@ if pop_thr>0
     den = n_r/(n_u*n_i);
     
     fprintf('\n');
+    fprintf('****************************************************** \n');
     fprintf('*** URM statistics after %g%% popular-item removal *** \n',100*pop_thr)
-    fprintf('Number of unqiue users: %g \n',n_u)
+    fprintf('****************************************************** \n');
+    fprintf('Number of unique users: %g \n',n_u)
     fprintf('Number of unique items: %g \n',n_i)
     fprintf('Number of ratings: %d \n',n_r)
     fprintf('Density of URM: %g \n',den)
@@ -61,29 +65,67 @@ fprintf('\n');
 fprintf('Split type: %s \n', split_type)
 fprintf('k-fold CV k=%g \n', k)
 
-     fold_size = floor(n_r/k);
-rating_indices = randperm(n_r); 
-
-for i = 1 : k
-    train_indices = rating_indices((i-1)*fold_size+1:i*fold_size);
-     test_indices = setdiff(rating_indices, train_indices)       ;
-        urm_train = urm(train_indices,:) ;
-         urm_test = urm(test_indices,:)  ;
+if strcmp(split_type,'rating')
+    
+    fold_size = floor(n_r/k);
+    rating_indices = randperm(n_r);
+    
+    for fld = 1 : k
+        test_indices_urmRows = rating_indices((fld-1)*fold_size+1:fld*fold_size);
+        train_indices_urmRows = setdiff(rating_indices, test_indices_urmRows)       ;
+        urm_train = urm(train_indices_urmRows,:) ;
+        urm_test = urm(test_indices_urmRows,:)  ;
+        
+        dup_ind = ismember(table2array(urm_train),table2array(urm_test),'rows');
+        if isempty(find(dup_ind, 1))
+            disp(['Fold ' num2str(fld) ': No duplicate entries (user,item,rating) found between train and test URMs.']);
+        else
+            disp(['Fold ' num2str(fld) ': A duplicate entries (user,item,rating) found between train and test URMs!'])
+            fprintf('\n')
+            disp('Check the following rows in urm_train:')
+            find(dup_ind)
+        end
+        
+        writetable(urm_train,fullfile(output_fold,['urm_train_split_type_' split_type '_fold' num2str(fld) 'of' num2str(k) '_pop_removed_' num2str(pop_thr) '.csv']))
+        writetable(urm_test,fullfile(output_fold,['urm_test_split_type_' split_type '_fold' num2str(fld) 'of' num2str(k) '_pop_removed_' num2str(pop_thr) '.csv']))
+        disp(['Fold ' num2str(fld) ' printed ...'])
+        
+    end
+    
+elseif strcmp(split_type,'item')
+    
+    unique_itemIds = unique(itemIds);
+               n_i = length(unique_itemIds);
+         fold_size = floor(n_i/k);
+      item_indices = randperm(n_i);
+    
+    for fld = 1 : k
+        test_itemIds = unique_itemIds(item_indices((fld-1)*fold_size+1:fld*fold_size));
+       train_itemIds = setdiff(unique_itemIds, test_itemIds)       ;
          
-         ind = ismember(table2array(urm_train),table2array(urm_test),'rows');
-         dup_ind = find(ind) ;
-         if isempty(dup_ind)
-             disp(['Fold ' num2str(i) ': No duplicate entries found between train and test URMs.']);           
-         else
-             disp(['Fold ' num2str(i) ': A duplicate entries found between train and test URMs!'])
-             fprintf('\n')
-             disp('Check the following rows in urm_train:')
-             dup_ind
-         end
-         
-         writetable(urm_train,fullfile(output_fold,['urm_train_split_type_' split_type '_fold' num2str(i) 'of' num2str(k) '_pop_removed_' num2str(pop_thr) '.csv']))
-         writetable(urm_test,fullfile(output_fold,['urm_test_split_type_' split_type '_fold' num2str(i) 'of' num2str(k) '_pop_removed_' num2str(pop_thr) '.csv']))
-         disp(['Fold ' num2str(i) ' printed ...'])
+        test_indices_urmRows = ismember(itemIds,test_itemIds);
+       train_indices_urmRows = not(test_indices_urmRows);
+       
+       
+       urm_train = urm(train_indices_urmRows,:)   ;
+        urm_test = urm(test_indices_urmRows,:)  ;
 
+         dup_ind = ismember(table2array(urm_train(:,2)),table2array(urm_test(:,2)));
+
+     if isempty(find(dup_ind, 1))
+         disp(['Fold ' num2str(fld) ': No duplicate item found between train and test URMs.']);
+     else
+         disp(['Fold ' num2str(fld) ': A duplicate item found between train and test URMs!']);
+         fprintf('\n');
+         disp('Check the following rows in urm_train:')
+         find(dup_ind)
+     end
+     
+        writetable(urm_train,fullfile(output_fold,['urm_train_split_type_' split_type '_fold' num2str(fld) 'of' num2str(k) '_pop_removed_' num2str(pop_thr) '.csv']))
+        writetable(urm_test,fullfile(output_fold,['urm_test_split_type_' split_type '_fold' num2str(fld) 'of' num2str(k) '_pop_removed_' num2str(pop_thr) '.csv']))
+        disp(['Fold ' num2str(fld) ' printed ...'])    
+        
+    end
 end
+
 
