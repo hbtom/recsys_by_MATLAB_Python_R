39156d29c659bec3cfb232c3b6d3d296199dcc53
yasdel <yashar.deldjoo@gmail.com>
2017-09-14 20:54:13 +0200

    Updated the MAP examples

--- MATLAB/recommenders/metrics/test_eval_metrics.m
+++ MATLAB/recommenders/metrics/test_eval_metrics.m
@@ -57,6 +57,22 @@ testCase = TestCase.forInteractiveUse;
  testCase.verifyThat(MAP_at_k(ranked_list_2,pos_items_ids,3),IsEqualTo(1/9,'Within', AbsoluteTolerance(1e-10)))
  testCase.verifyThat(MAP_at_k(ranked_list_2,pos_items_ids,5),IsEqualTo(43/120,'Within', AbsoluteTolerance(1e-10)))
 
- 
+ pos_items_ids = [1, 3, 5, 10];
+ ranked_list_1 = [1, 3, 5, 4, 9];
+ ranked_list_2 = [1, 3, 4, 9, 5];
+ ranked_list_3 = [4, 9, 1, 3, 5];
+ ranked_list_4 = [12, 11, 13, 17, 15, 16, 1, 3, 5, 10];
+ ranked_list_5 = [1, 11, 12, 13, 14, 15, 4, 5, 10, 16];
+ ranked_list_6 = [1, 3, 12, 13, 14, 15, 11,  10,8, 16];
+       
+
+ testCase.verifyThat(MAP_at_k(ranked_list_1,pos_items_ids,5),IsEqualTo((1/1 + 2/2 + 3/3) / 4,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(MAP_at_k(ranked_list_2,pos_items_ids,5),IsEqualTo((1/1 + 2/2 + 3/5) / 4,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(MAP_at_k(ranked_list_3,pos_items_ids,5),IsEqualTo((1/3 + 2/4 + 3/5) / 4,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(MAP_at_k(ranked_list_4,pos_items_ids,10),IsEqualTo((1/7 + 2/8 + 3/9 + 4/10)/ 4,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(MAP_at_k(ranked_list_5,pos_items_ids,10),IsEqualTo((1/1 + 2/8 + 3/9)/ 4,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(MAP_at_k(ranked_list_6,pos_items_ids,10),IsEqualTo((1/1 + 2/2 + 3/8)/ 4,'Within', AbsoluteTolerance(1e-10)))
+
+
  
  
