7e29b1d2f3e16a5d829da077c7bc94f09c62b4ed
yasdel <yashar.deldjoo@gmail.com>
2017-09-19 21:28:06 +0200

    Added NDCG metric

---
+++ MATLAB/recommenders/metrics/NDCG.m
@@ -0,0 +1,58 @@
+function output = NDCG(ranked_list,pos_items_ids,pos_rel_scores,gain_factor)
+
+% The function computes Recall metric at particular cut_off value k.
+%  Inputs: 
+%            ranked_list: ranked recommendation list
+%         pos_items_ids : Ids of positive items
+%        pos_rel_scores : relevance score for positive items(e.g., ratings)
+%                    k  : cut_off value 
+% Outputs:
+%               output  : recall score at k
+% Example: 
+%     ranked_list_1 = [2 1 5 4 3]    -> DCG1 = 5/log2(3)+5/log2(4)+4/log2(5)+4/log2(6)
+%     ranked_list_2 = [2 4 3 1 5]    -> DCG2 = 4/log2(3)+4/log2(4)+5/log2(5)+5/log2(6)
+%     ranked_list_3 = [10 8 3 5 1]   -> DCG3 = 4/log2(4)+5/log2(5)+5/log2(6)
+%     pos_items_ids = [1 5 4 3]      -> IDCG = 5/log2(2)+5/log2(3)+4/log2(4)+4/log2(5)
+%     pos_relevance_scores = [5 5 4 4] 
+%    
+%     ratings = [5 2 4 4 5 3 2 na 3 na]  
+%     pos_items_ids = [1 5 4 3] 
+%
+% Yashar Deldjoo
+% Sep 14, 2017 
+% Politecnico di Milano, Italy
+
+IDCG = 0 ;
+
+for i = 1 : length(pos_items_ids)
+    if strcmp(gain_factor,'exp')
+        gain = 2^pos_rel_scores(i)-1 ;
+    elseif strcmp(gain_factor,'rating')
+        gain = pos_rel_scores(i) ;        
+    end
+    IDCG = IDCG +  gain/log2(i+1);
+end
+
+DCG = 0 ;
+
+for j = 1 : length(ranked_list)
+    ind = find(pos_items_ids==ranked_list(j));
+    if ~isempty(ind) 
+        if strcmp(gain_factor,'exp')
+            gain = 2^pos_rel_scores(ind)-1 ;
+        elseif strcmp(gain_factor,'rating')
+            gain = pos_rel_scores(ind) ;
+        end
+        DCG = DCG +  gain/log2(j+1);
+        
+    end
+end
+
+NDCG = DCG/IDCG;
+
+assert((0<=NDCG) && (NDCG<=1),'The condition 0<=NDCG<=1 did NOT hold!');
+
+output = NDCG ;
+
+end
+
--- MATLAB/recommenders/metrics/test_eval_metrics.m
+++ MATLAB/recommenders/metrics/test_eval_metrics.m
@@ -1,3 +1,9 @@
+% This is the script for testing recommender systems evluation metrics. 
+% Yashar Deldjoo
+% September 2017 
+% Politecnico di Milano, Italy
+% Johannes Kepler University, Linz, Austria
+
 clear
 clc
 close all
@@ -8,10 +14,6 @@ import matlab.unittest.constraints.AbsoluteTolerance
 
 testCase = TestCase.forInteractiveUse;
 
-% This is the script for testing recommender systems evluation metrics. 
-% Yashar Deldjoo
-% Sep 2017 
-% Politecnico di Milano, Italy
 
  ranked_list_1 = [2 5 1 4 3] ;
  ranked_list_2 = [10 8 3 5 1]; 
@@ -51,11 +53,11 @@ testCase = TestCase.forInteractiveUse;
  fprintf('............................................. \n')
  fprintf('\n')
 
- fprintf('Testing MAP ... \n')
- testCase.verifyThat(MAP_at_k(ranked_list_1,pos_items_ids,3),IsEqualTo(7/18,'Within', AbsoluteTolerance(1e-10)))
- testCase.verifyThat(MAP_at_k(ranked_list_1,pos_items_ids,5),IsEqualTo(163/240,'Within', AbsoluteTolerance(1e-10)))
- testCase.verifyThat(MAP_at_k(ranked_list_2,pos_items_ids,3),IsEqualTo(1/9,'Within', AbsoluteTolerance(1e-10)))
- testCase.verifyThat(MAP_at_k(ranked_list_2,pos_items_ids,5),IsEqualTo(43/120,'Within', AbsoluteTolerance(1e-10)))
+ fprintf('Testing AP ... \n')
+ testCase.verifyThat(AP_at_k(ranked_list_1,pos_items_ids,3),IsEqualTo(7/18,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(AP_at_k(ranked_list_1,pos_items_ids,5),IsEqualTo(163/240,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(AP_at_k(ranked_list_2,pos_items_ids,3),IsEqualTo(1/9,'Within', AbsoluteTolerance(1e-10)))
+ testCase.verifyThat(AP_at_k(ranked_list_2,pos_items_ids,5),IsEqualTo(43/120,'Within', AbsoluteTolerance(1e-10)))
 
  pos_items_ids = [1, 3, 5, 10];
  ranked_list_1 = [1, 3, 5, 4, 9];
@@ -73,6 +75,13 @@ testCase = TestCase.forInteractiveUse;
  testCase.verifyThat(AP_at_k(ranked_list_5,pos_items_ids,10),IsEqualTo((1/1 + 2/8 + 3/9)/ 4,'Within', AbsoluteTolerance(1e-10)))
  testCase.verifyThat(AP_at_k(ranked_list_6,pos_items_ids,10),IsEqualTo((1/1 + 2/2 + 3/8)/ 4,'Within', AbsoluteTolerance(1e-10)))
 
+  %%
+ fprintf('\n')
+ fprintf('............................................. \n')
+ fprintf('\n')
+
+ fprintf('Testing RR ... \n')
+ 
  pos_items_ids = [1 5 4 3];
  ranked_list_1 = [2 5 1 4 3];
  ranked_list_2 = [10 8 3 5 1];
@@ -85,7 +94,36 @@ testCase = TestCase.forInteractiveUse;
  testCase.verifyThat(RR_at_k(ranked_list_2,pos_items_ids,5),IsEqualTo((1/3),'Within', AbsoluteTolerance(1e-10)))
  testCase.verifyThat(RR_at_k(ranked_list_3,pos_items_ids,3),IsEqualTo((0),'Within', AbsoluteTolerance(1e-10)))
  testCase.verifyThat(RR_at_k(ranked_list_3,pos_items_ids,5),IsEqualTo((1/4),'Within', AbsoluteTolerance(1e-10)))
+ %%
+ fprintf('\n')
+ fprintf('............................................. \n')
+ fprintf('\n')
 
+ fprintf('Testing NDCG ... \n')
+ 
+ranked_list_1 = [2 1 5 4 3];   
+ranked_list_2 = [2 4 3 1 5];  
+ranked_list_3 = [10 8 3 5 1];
+pos_items_ids = [1 5 4 3];
+pos_relevance_scores = [5 5 4 4];
+
+IDCG = 5/log2(2)+5/log2(3)+4/log2(4)+4/log2(5);  % rating gain
+DCG1 = 5/log2(3)+5/log2(4)+4/log2(5)+4/log2(6);  % rating gain
+DCG2 = 4/log2(3)+4/log2(4)+5/log2(5)+5/log2(6);  % rating gain
+DCG3 = 4/log2(4)+5/log2(5)+5/log2(6);            % rating gain
+
+testCase.verifyThat(NDCG(ranked_list_1,pos_items_ids,pos_relevance_scores,'rating'),IsEqualTo((DCG1/IDCG),'Within', AbsoluteTolerance(1e-10)))
+testCase.verifyThat(NDCG(ranked_list_2,pos_items_ids,pos_relevance_scores,'rating'),IsEqualTo((DCG2/IDCG),'Within', AbsoluteTolerance(1e-10)))
+testCase.verifyThat(NDCG(ranked_list_3,pos_items_ids,pos_relevance_scores,'rating'),IsEqualTo((DCG3/IDCG),'Within', AbsoluteTolerance(1e-10)))
+
+IDCG = (2^5-1)/log2(2)+(2^5-1)/log2(3)+(2^4-1)/log2(4)+(2^4-1)/log2(5);  % exp gain
+DCG1 = (2^5-1)/log2(3)+(2^5-1)/log2(4)+(2^4-1)/log2(5)+(2^4-1)/log2(6);  % exp gain
+DCG2 = (2^4-1)/log2(3)+(2^4-1)/log2(4)+(2^5-1)/log2(5)+(2^5-1)/log2(6);  % exp gain
+DCG3 = (2^4-1)/log2(4)+(2^5-1)/log2(5)+(2^5-1)/log2(6);            % exp gain
+
+testCase.verifyThat(NDCG(ranked_list_1,pos_items_ids,pos_relevance_scores,'exp'),IsEqualTo((DCG1/IDCG),'Within', AbsoluteTolerance(1e-10)))
+testCase.verifyThat(NDCG(ranked_list_2,pos_items_ids,pos_relevance_scores,'exp'),IsEqualTo((DCG2/IDCG),'Within', AbsoluteTolerance(1e-10)))
+testCase.verifyThat(NDCG(ranked_list_3,pos_items_ids,pos_relevance_scores,'exp'),IsEqualTo((DCG3/IDCG),'Within', AbsoluteTolerance(1e-10)))
 
 
 
