aac1ac3d3edf380d41b916bedc91d0745c466350
yasdel <yashar.deldjoo@gmail.com>
2017-10-01 18:30:06 +0200

    Added random recommender and a big fixed
    A bug fixed in evaluate_urm function in the sense that it was caculating the true positive items based on the ones in the first user's list. Also a random recommender added.

--- MATLAB/recommenders/algorithms/demo_CB_ItemItem_Knn.m
+++ MATLAB/recommenders/algorithms/demo_CB_ItemItem_Knn.m
@@ -22,7 +22,9 @@ close all
 %% PATHs 
 
 addpath('/Users/yashar/Documents/GitHub/recsys_by_MATLAB_Python_R/MATLAB/utils/rec');
+addpath('/Users/yashar/Documents/GitHub/recsys_by_MATLAB_Python_R/MATLAB/recommenders/metrics');
 rootAddr = '/Users/yashar/OneDrive - Politecnico di Milano/data/';
+ outAddr = '';
 
 %% LOAD URMs and ICMs
 
@@ -32,6 +34,11 @@ col1_name = 'userId' ;
 col2_name = 'movieId';
 col3_name = 'rating' ;
 sim_type  = 'cosine' ;
+
+% feature specific params
+feature_name = 'audio_ivec' ;
+    gmm_size = 128;       % small = [128,256]  big = [16,32,64,128,256,512]
+       tvDim = 40 ;       % small = [40,100, 200];
 
 trainRatings = readtable(fullfile(rootAddr,'urms',['urm_train_split_type_item_fold' num2str(fold_no) 'of5_pop_removed_0.csv']));
 
@@ -51,31 +58,31 @@ trainRatings_New_tr = output_tr.inputRating_New ;
        urmTest_New  = sparse(inputRating_New_te.new_userId,inputRating_New_te.new_movieId,inputRating_New_te.rating);  % urmTest = [n_u2,n_i2]
                       stats_about_URm(inputRating_New_te,'testRatings',['new_' col1_name],['new_' col2_name],col3_name);
 
-    load(fullfile(rootAddr,'ivec','train_test_seperated','final_ivec_data_with_genre',['IVecTableFinal_with_genre_label_sitem_fold_' num2str(fold_no) '_gmm_16_tvDim_10.mat']))
-                ICM = IVecTable_with_genre_label(:,1:11);
+    load(fullfile(rootAddr,'ivec','train_test_seperated','final_ivec_data_with_genre',['IVecTableFinal_with_genre_label_sitem_fold_' num2str(fold_no) '_gmm_' num2str(gmm_size) '_tvDim_' num2str(tvDim) '.mat']))
+                ICM = IVecTable_with_genre_label(:,1:tvDim+1);
 
-% The function 'prepare_distance_3tuple' prepares a similarity array of form [item_i,item_j,sim_score] which contains the
-% pairwise similarities between each pair of items for item in ICM. The first column of the matrix is assumed to contain
-% item ids.
+The function 'prepare_distance_3tuple' prepares a similarity array of form [item_i,item_j,sim_score] which contains the
+pairwise similarities between each pair of items for item in ICM. The first column of the matrix is assumed to contain
+item ids.
 
-        % distArray = prepare_distance_3tuple(feature_table,sim_type,col1_name)
+        distArray = prepare_distance_3tuple(feature_table,sim_type,col1_name)
           distArray = prepare_distance_3tuple(ICM,sim_type,col2_name);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          
-% user_Id2idx_tr = [userId, new_userId], item_Id2idx_tr = [itemId, new_itemId]
-% 
-% Note 1:
-% The "userIds" in both 'user_Id2idx_tr' and 'user_Id2idx_te' are the same. The same applies for "itemIds" in both 
-% 'item_Id2idx_tr' and 'item_Id2idx_tr'. These Ids can be used for finding similar items and users in both dataset.
-
-% Note 2:
-% The row and column indices in 'urmTrain_New' after applying the function "prepare_ratingMat_Id2ind" are similar to 
-% 'new_userIds' and 'new_itemIds' in 'user_Id2idx_tr' and 'item_Id2idx_tr'. In a similar manner, the row and column 
-% indices in "urmTest_New" match with 'new_userIds' and 'new_itemIds' in 'user_Id2idx_te' and 'item_Id2idx_te.'
-%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          
+user_Id2idx_tr = [userId, new_userId], item_Id2idx_tr = [itemId, new_itemId]
 
-%%
+Note 1:
+The "userIds" in both 'user_Id2idx_tr' and 'user_Id2idx_te' are the same. The same applies for "itemIds" in both 
+'item_Id2idx_tr' and 'item_Id2idx_tr'. These Ids can be used for finding similar items and users in both dataset.
+
+Note 2:
+The row and column indices in 'urmTrain_New' after applying the function "prepare_ratingMat_Id2ind" are similar to 
+'new_userIds' and 'new_itemIds' in 'user_Id2idx_tr' and 'item_Id2idx_tr'. In a similar manner, the row and column 
+indices in "urmTest_New" match with 'new_userIds' and 'new_itemIds' in 'user_Id2idx_te' and 'item_Id2idx_te.'
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          
+
+%
 
                    [test_useridx,test_itemidx] = find(urmTest_New ~=0);
                    
@@ -86,7 +93,7 @@ trainRatings_New_tr = output_tr.inputRating_New ;
                    urmPred_weightedAvg_skg0001 = sparse(zeros(size(urmTest_New)));
 
 
-             % Inititate the class properties and object
+             Inititate the class properties and object
              recommender_Object                = CBF_ItemItem_Knn  ;
              recommender_Object.urmTrain_New   = urmTrain_New      ;
              recommender_Object.distArray      = distArray         ;
@@ -94,25 +101,25 @@ trainRatings_New_tr = output_tr.inputRating_New ;
              recommender_Object.item_Id2idx_tr = item_Id2idx_tr    ;
              recommender_Object.nn             = 10                ;
              
-% Since the module is an item-wise operation (i.e., item-item KNN), for "computaional efficieny" we do the processing of
-% rating prediction "item-wise".
+Since the module is an item-wise operation (i.e., item-item KNN), for "computaional efficieny" we do the processing of
+rating prediction "item-wise".
 tic
 for item_no = 1 : size(urmTest_New,2)
     
        int_ind = (test_itemidx == item_no) ;
     
-    % Note 3:
-    % 'test_useridx' and 'test_itemidx' contain internal indices of 'urmTest_New' which is equivalent to 'new_userIds' and 
-    % 'new_itemIds' which can be found in 'user_Id2idx_te' and 'item_Id2idx_te.'
+    Note 3:
+    'test_useridx' and 'test_itemidx' contain internal indices of 'urmTest_New' which is equivalent to 'new_userIds' and 
+    'new_itemIds' which can be found in 'user_Id2idx_te' and 'item_Id2idx_te.'
     
-    % For us, we need to use the true 'userIds' and 'itemIds' which are the same in both train and test datasets. Whenver 'new'
-    % is not mentioned, it mean they refer to "true" user and item Ids. In addition, 'userId_te' and 'itemId_te' contains both 
-    % true user and item Ids.
+    For us, we need to use the true 'userIds' and 'itemIds' which are the same in both train and test datasets. Whenver 'new'
+    is not mentioned, it mean they refer to "true" user and item Ids. In addition, 'userId_te' and 'itemId_te' contains both 
+    true user and item Ids.
     
-    % user_Id2idx_te = [userId, new_userId], item_Id2idx_te = [itemId, new_itemId]
+    user_Id2idx_te = [userId, new_userId], item_Id2idx_te = [itemId, new_itemId]
     
        
-        % Find the true userIds and itemIds to path it to the recommender
+        Find the true userIds and itemIds to path it to the recommender
          itemId_te = item_Id2idx_te(table2array(item_Id2idx_te(:,2)) == item_no,1);
         userIds_te = user_Id2idx_te(ismember(table2array(user_Id2idx_te(:,2)),test_useridx(int_ind)),1);
    
@@ -136,14 +143,23 @@ for item_no = 1 : size(urmTest_New,2)
          if mod(item_no,250) == 1
              fprintf('Movie %d \n',item_no)
          end
-%     if length(table2array(userIds_te)) ~= length(output.rating_pred_avg)
-%         disp('Opps!')
-%     end
+    if length(table2array(userIds_te)) ~= length(output.rating_pred_avg)
+        disp('Opps!')
+    end
 end
 
 toc
 
-save(['RecSys_Result_fold_no' num2str(fold_no) '.mat'],'urmTest_New','urmPred_Avg', ...
-    'urmPred_weightedAvg','urmPred_weightedAvg_skg01','urmPred_weightedAvg_skg001','urmPred_weightedAvg_skg0001');
+if strcmp(feature_name,'audio_ivec')
+    save(['RecSys_results_feature_name' feature_name '_gmm_size_' num2str(gmm_size) '_tvDim_' num2str(tvDim) '_fold_' num2str(fold_no) 'of5.mat'],'urmTest_New','urmPred_Avg', ...
+        'urmPred_weightedAvg','urmPred_weightedAvg_skg01','urmPred_weightedAvg_skg001','urmPred_weightedAvg_skg0001');  
+end
 
-    
+       load('RecSys_Result_fold_no1.mat')
+output = evaluate_urms(urmTest_New, urmPred_Avg);
+outputRandom_rec = evaluate_urms_random(urmTest_New, urmPred_Avg);
+
+if strcmp(feature_name,'audio_ivec')
+    % for i-vec
+    save(['Evaluation_results_feature_name' feature_name '_gmm_size_' num2str(gmm_size) '_tvDim_' num2str(tvDim) '_fold_' num2str(fold_no) 'of5.mat'],'output');    
+end
---
+++ MATLAB/recommenders/algorithms/evaluate_urms.m
@@ -0,0 +1,121 @@
+function output = evaluate_urms(urmTest, urmPred)
+
+fprintf('Evaluation strtated ...  \n');
+
+p = 0;
+for u = 1 : size(urmTest,1)
+    if nnz(urmPred(u,:)) == nnz(urmTest(u,:))
+                
+        [rp_u,ranked_list] = sort(urmPred(u,:)-0.1+0.2*rand(1,size(urmTest,2)),'descend');
+             pos_items_ids = find(urmTest(u,:)>3);
+             
+             if isempty(pos_items_ids)
+                continue 
+             end
+             
+             if mod(p,2500)==1
+                fprintf('%d true users evaluated \n',p) 
+             end
+                 
+              p = p + 1;
+
+              PR1_u(p) = precision_at_k(ranked_list,pos_items_ids,1) ;
+              PR3_u(p) = precision_at_k(ranked_list,pos_items_ids,3) ;
+              PR4_u(p) = precision_at_k(ranked_list,pos_items_ids,4) ;
+              PR5_u(p) = precision_at_k(ranked_list,pos_items_ids,5) ;
+              PR10_u(p) = precision_at_k(ranked_list,pos_items_ids,10) ;
+              PR20_u(p) = precision_at_k(ranked_list,pos_items_ids,20) ;
+              PR30_u(p) = precision_at_k(ranked_list,pos_items_ids,30) ;
+              PR50_u(p) = precision_at_k(ranked_list,pos_items_ids,50) ;        
+              PR100_u(p) = precision_at_k(ranked_list,pos_items_ids,100) ;
+
+             
+              Re1_u(p) = recall_at_k(ranked_list,pos_items_ids,1) ;
+              Re3_u(p) = recall_at_k(ranked_list,pos_items_ids,3) ;
+              Re4_u(p) = recall_at_k(ranked_list,pos_items_ids,4) ;
+              Re5_u(p) = recall_at_k(ranked_list,pos_items_ids,5) ;
+             Re10_u(p) = recall_at_k(ranked_list,pos_items_ids,10) ;
+             Re20_u(p) = recall_at_k(ranked_list,pos_items_ids,20) ;
+             Re30_u(p) = recall_at_k(ranked_list,pos_items_ids,30) ;
+             Re50_u(p) = recall_at_k(ranked_list,pos_items_ids,50) ;
+            Re100_u(p) = recall_at_k(ranked_list,pos_items_ids,100) ;
+            
+             MAP1_u(p) = AP_at_k(ranked_list,pos_items_ids,1);
+             MAP3_u(p) = AP_at_k(ranked_list,pos_items_ids,3);
+             MAP4_u(p) = AP_at_k(ranked_list,pos_items_ids,4);
+             MAP5_u(p) = AP_at_k(ranked_list,pos_items_ids,5);
+            MAP10_u(p) = AP_at_k(ranked_list,pos_items_ids,10);
+            MAP20_u(p) = AP_at_k(ranked_list,pos_items_ids,20);
+            MAP30_u(p) = AP_at_k(ranked_list,pos_items_ids,30);
+            MAP50_u(p) = AP_at_k(ranked_list,pos_items_ids,50);
+           MAP100_u(p) = AP_at_k(ranked_list,pos_items_ids,100);
+
+            NDCG1_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,1,'exp');
+            NDCG3_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,3,'exp');
+            NDCG4_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,4,'exp');
+            NDCG5_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,5,'exp');
+           NDCG10_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,10,'exp');
+           NDCG20_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,20,'exp');
+           NDCG30_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,30,'exp');
+           NDCG50_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,50,'exp');
+          NDCG100_u(p) = NDCG(ranked_list,pos_items_ids,rp_u,100,'exp'); 
+          
+          MRR1_u(p) =  RR_at_k(ranked_list,pos_items_ids,1);
+          MRR3_u(p) =  RR_at_k(ranked_list,pos_items_ids,3);
+          MRR4_u(p) =  RR_at_k(ranked_list,pos_items_ids,4);
+          MRR5_u(p) =  RR_at_k(ranked_list,pos_items_ids,5);
+          MRR10_u(p) =  RR_at_k(ranked_list,pos_items_ids,10);
+          MRR20_u(p) =  RR_at_k(ranked_list,pos_items_ids,20);
+          MRR30_u(p) =  RR_at_k(ranked_list,pos_items_ids,30);
+          MRR50_u(p) =  RR_at_k(ranked_list,pos_items_ids,50);
+          MRR100_u(p) =  RR_at_k(ranked_list,pos_items_ids,100);
+
+        
+    end
+end
+
+fprintf('Evaluation completed! %d evaluated out of %d \n',p,size(urmTest,1))
+
+Precision_all = [];
+   Recall_all = [];
+       F1_all = [];
+      MAP_all = [];
+     NDCG_all = [];
+      MRR_all = [];
+       
+for cut_off = [1 3 4 5 10 20 30 50 100]
+   eval(['output.Precision_' num2str(cut_off) '= mean(PR' num2str(cut_off) '_u);'])  
+   eval(['output.Recall_' num2str(cut_off) '= mean(Re' num2str(cut_off) '_u);'])  
+   eval(['output.F1_' num2str(cut_off) '= (' num2str(2) '*output.Precision_' num2str(cut_off) '*output.Recall_' num2str(cut_off)...
+       ')/(' 'output.Precision_' num2str(cut_off) '+' 'output.Recall_' num2str(cut_off) ');'])  
+   eval(['output.MAP_' num2str(cut_off) '= mean(MAP' num2str(cut_off) '_u);'])  
+   eval(['output.NDCG_' num2str(cut_off) '= mean(NDCG' num2str(cut_off) '_u);'])  
+   eval(['output.MRR_' num2str(cut_off) '= mean(MRR' num2str(cut_off) '_u);'])  
+   
+end
+for cut_off = [1 3 4 5 10 20 30 50 100]
+      eval(['Precision_all = [Precision_all ' 'output.Precision_' num2str(cut_off) '];'])  
+      eval(['Recall_all    = [Recall_all ' 'output.Recall_' num2str(cut_off) '];'])  
+      eval(['F1_all = [F1_all ' 'output.F1_' num2str(cut_off) '];'])  
+      eval(['MAP_all = [MAP_all ' 'output.MAP_' num2str(cut_off) '];'])  
+      eval(['NDCG_all = [NDCG_all ' 'output.NDCG_' num2str(cut_off) '];'])  
+      eval(['MRR_all = [MRR_all ' 'output.MRR_' num2str(cut_off) '];'])  
+
+    
+end
+
+output.Precision_all= Precision_all;
+output.Recall_all = Recall_all ;
+output.F1_all = F1_all;
+output.MAP_all = MAP_all;
+output.NDCG_all = NDCG_all;
+output.MRR_all = MRR_all;
+end
+
+
+
+
+
+
+
+
