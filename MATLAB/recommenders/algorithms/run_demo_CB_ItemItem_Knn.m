a9dd01e5518cf8868cf0668bc030ae37f6a17eba
yasdel <yashar.deldjoo@gmail.com>
2017-10-04 13:49:24 +0200

    conflict resolved!

---
+++ MATLAB/recommenders/algorithms/run_demo_CB_ItemItem_Knn.m
@@ -0,0 +1,19 @@
+function []=run_demo_CB_ItemItem_Knn(feature_name,rand_rec_ON,nn,gmm_size,tvDim)
+
+% feature_name = 'genre'; 'audio_ivec'
+% rand_rec_ON = 'YES';
+
+
+for gmm_size = gmm_size
+    for tvDim = tvDim
+        for fold_no = 1 : 5
+            feature_name
+            rand_rec_ON
+            [gmm_size,tvDim,nn]
+            demo_CB_ItemItem_Knn
+        end
+    end
+end
+
+    %     gmm_size = 128;       % small = [128,256]  big = [16,32,64,128,256,512]
+%        tvDim = 40 ;       % small = [40,100, 200];
